47151ab2a51b8e12bbf42461c818854a9ec84814
rodrigo11gg <castanedarodrigo609@gmail.com>
2025-05-30 18:23:12 -0500

    practca

---
+++ pregunta5.py
@@ -0,0 +1,4 @@
+#En este ejercicio, tendrán total libertad para elegir la problemática y la mejor solución. 
+# Cada grupo podrá decidir su propio enfoque y desarrollar el software según sus criterios. 
+# El grupo que presente la mejor solución será el único en recibir los 60 puntos asignados a esta pregunta.
+#¡Buena suerte!
