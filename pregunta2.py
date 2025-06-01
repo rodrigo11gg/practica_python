37c4e426b992e1d1142a93e13ec6a1e0f95ce6bb
rodrigo11gg <castanedarodrigo609@gmail.com>
2025-06-01 10:40:20 -0500

    realizadoejercicio2

--- pregunta2.py
+++ pregunta2.py
@@ -2,3 +2,11 @@
 # y tener unos ingresos iguales o superiores a 1000 € mensuales. 
 # Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales 
 # y muestre por pantalla si el usuario tiene que tributar o no. (10p)
+#realizado por rodrigo
+
+edad = int(input("Introduce tu edad: "))
+ingresos = float(input("Introduce tus ingresos mensuales: "))
+if edad > 16 and ingresos >= 1000:
+    print("Tienes que tributar.")
+else:
+    print("No tienes que tributar.")
