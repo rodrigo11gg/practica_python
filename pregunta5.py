f2d936fbef01017be29687ea825658e4e941a127
rodrigo11gg <castanedarodrigo609@gmail.com>
2025-06-05 15:55:19 -0500

    ultimos

--- pregunta5.py
+++ pregunta5.py
@@ -2,3 +2,19 @@
 # Cada grupo podrá decidir su propio enfoque y desarrollar el software según sus criterios. 
 # El grupo que presente la mejor solución será el único en recibir los 60 puntos asignados a esta pregunta.
 #¡Buena suerte!
+import pyautogui as spam
+from time import sleep
+
+limitmsg = input('Número de mensajes: ')
+msg = input('Mensaje: ')
+
+if not limitmsg.isdigit():
+    print("Por favor ingresa un número válido.")
+    exit()
+
+sleep(3)  
+
+for i in range(int(limitmsg)):
+    spam.typewrite(msg)
+    spam.press('enter')
+    sleep(0.4) 
