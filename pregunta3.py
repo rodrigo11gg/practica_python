4dbff8bd3e11b33d6379d27af6a1917ab4fe9edd
Astry0805 <aipanaquepalacios@gmail.com>
2025-06-04 21:33:15 -0500

    ejercicio 3 y 4

--- pregunta3.py
+++ pregunta3.py
@@ -15,3 +15,15 @@
 #Escribir un programa que pregunte al usuario su renta anual 
 # y muestre por pantalla el tipo impositivo que le corresponde. (10p)
 
+renta=float(input("Introduce tu renta anual: "))
+
+if renta<10000:
+    print("Su tipo impositivo es de 5% ")
+elif 10000<= renta <20000:
+    print("Su tipo impositivo es de 15%")
+elif 20000<= renta <35000:
+    print("Su tipo impositivo es de 20%")
+elif 35000<= renta <60000:
+    print("Su tipo impositivo es de 30%")
+else:
+    print ("Su tipo impositivo es de 45%")
--- pregunta4.py
+++ pregunta4.py
@@ -3,3 +3,15 @@
 # El programa debe preguntar al usuario la edad del cliente y mostrar el precio de la entrada. 
 # Si el cliente es menor de 4 años puede entrar gratis, si tiene entre 4 y 18 años debe pagar
 #  5€ y si es mayor de 18 años, 10€. (10p)
+
+print("🎮 Bienvenidos a nuestra sala de juegos 🎮")
+edad= int(input("Introduce tu edad: "))
+
+if edad<4:
+    print("Tu entrada es gratis")
+elif 4<= edad <18:
+    print("El precio de la entrada es 5€")
+else: 
+    print("El precio de la entrada es 10€")
+    
+print("¡Que te diviertas al máximo!🎮😄")
